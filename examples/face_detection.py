cc98ef846060fee5e351d2b732145c74e9ffa9d6
PICHHAPATI VISHNU VARDHAN REDDY <109100018+vishnupichhapati@users.noreply.github.com>
2022-08-01 21:39:01 +0530

    Update face_detection.py

--- examples/face_detection.py
+++ examples/face_detection.py
@@ -1,15 +1,15 @@
 import argparse
 import io
-
+import streamlit
 from google.cloud import vision
 from google.cloud.vision import types
-
-
+image_file = st.file_uploader("Choose an imamge file")
 def main(image_file):
     # Instantiates a client
     client = vision.ImageAnnotatorClient()
 
     # Loads the image into memory
+    
     with io.open(image_file, 'rb') as image_file:
         content = image_file.read()
 
@@ -26,4 +26,4 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('image_file', help='The image you\'d like to label.')
     args = parser.parse_args()
-    main(args.image_file)
+    st.image(main(args.image_file))
