bfbc3f919e88bf9efb15a788d4e82603403395a2
PICHHAPATI VISHNU VARDHAN REDDY <109100018+vishnupichhapati@users.noreply.github.com>
2022-08-01 21:46:57 +0530

    Update face_detection.py

--- examples/face_detection.py
+++ examples/face_detection.py
@@ -4,26 +4,27 @@ import streamlit as st
 from google.cloud import vision
 from google.cloud.vision import types
 image_file = st.file_uploader("Choose an imamge file")
-def main(image_file):
+button=st.button('Click me',help='To give the image')
+if image_file and buttom:
+    def main(image_file):
     # Instantiates a client
-    client = vision.ImageAnnotatorClient()
+        client = vision.ImageAnnotatorClient()
 
     # Loads the image into memory
     
-    with io.open(image_file, 'rb') as image_file:
-        content = image_file.read()
+        with io.open(image_file, 'rb') as image_file:
+            content = image_file.read()
 
-    image = types.Image(content=content)
+        image = types.Image(content=content)
 
     # Performs label detection on the image file
-    response = client.face_detection(image=image)
-    labels = response.face_annotations
-    for label in labels:
-        print('Joy Likelihood: {}'.format(label.joy_likelihood))
+        response = client.face_detection(image=image)
+        labels = response.face_annotations
+        for label in labels:
+            print('Joy Likelihood: {}'.format(label.joy_likelihood))
 
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('image_file', help='The image you\'d like to label.')
-    args = parser.parse_args()
-    st.image(main(args.image_file))
+    if __name__ == '__main__':
+        parser = argparse.ArgumentParser()
+        parser.add_argument('image_file', help='The image you\'d like to label.')
+        args = parser.parse_args()
+        st.image(main(args.image_file))
