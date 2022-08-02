06b6a6e3d84a626265cf5c95ff48328029585a5a
PICHHAPATI VISHNU VARDHAN REDDY <109100018+vishnupichhapati@users.noreply.github.com>
2022-08-02 17:21:32 +0530

    Update file_detection.py

--- examples/file_detection.py
+++ examples/file_detection.py
@@ -42,6 +42,8 @@ def load_image(image_file):
 
 
 def main():
+	image = "https://ninjaforms.com/extensions/file-uploads/"
+	st.image(image, use_column_width=True)
 	st.title("UPLOAD FILE")
 
 	menu = ["Image","Dataset","DocumentFiles","Details About"]
